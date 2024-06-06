45626b5130498a281c0d2fb6de42941ae23df24d
Daniela Plascencia <daniela.plascencia@canonical.com>
2024-06-06 12:40:43 +0200

    chore: replace ROCKs with rocks
    This commit replaces ROCKs with rocks to be in sync with Canonical's standard terminology.
    
    Part of canonical/bundle-kubeflow#916

--- metacontroller/tests/test_rock.py
+++ metacontroller/tests/test_rock.py
@@ -13,7 +13,7 @@ def test_rock():
     check_rock = CheckRock("rockcraft.yaml")
     rock_image = check_rock.get_name()
     rock_version = check_rock.get_version()
-    LOCAL_ROCK_IMAGE = f"{rock_image}:{rock_version}"
+    LOCAL_rock_IMAGE = f"{rock_image}:{rock_version}"
 
     # assert the rock contains the expected files
     subprocess.run(
@@ -21,7 +21,7 @@ def test_rock():
             "docker",
             "run",
             "--rm",
-            LOCAL_ROCK_IMAGE,
+            LOCAL_rock_IMAGE,
             "exec",
             "ls",
             "-la",
